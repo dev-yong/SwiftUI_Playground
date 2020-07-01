f9e6c7b5aa43c9d13844d4394ea666b96e906367
dev-yong <rhkdrmfh@gmail.com>
2020-07-01 23:06:17 +0900

    no message

---
+++ Pages/Page 1.xcplaygroundpage/Contents.swift
@@ -0,0 +1,13 @@
+//: [Previous](@previous)
+
+import Combine
+import SwiftUI
+import PlaygroundSupport
+
+let view = VStack {
+    
+    Text("Hello, world")
+}
+PlaygroundPage.current.liveView = UIHostingController(rootView: view)
+
+//: [Next](@next)
